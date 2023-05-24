2ae780f4616ad4b5b34fca0049877445ea133a90
keiitikanemoto <lincolnkanemoto@zohomail.com>
2023-05-24 11:43:02 -0300

    Nova pasta Adicionada

---
+++ Projeto ContaBanco/ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,23 @@
+import java.text.Format;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o número da agência: ");
+        int agencia = sc.nextInt();
+
+        System.out.println("Digite o número da conta: ");
+        int conta = sc.nextInt();
+
+        System.out.println("Digite o nome de usuário: ");
+        String nome = sc.nextLine();
+
+        double saldo = 237.48;
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %d, conta %d e seu saldo R$ %f já esdtá disponível para saque.", nome, agencia, conta, saldo);
+
+    }
+}
