0e72341ab24664beb16fd202f8c9f0ef29586201
keiitikanemoto <lincolnkanemoto@zohomail.com>
2023-05-24 12:46:54 -0300

    Aula de Condionais

---
+++ ControleDeFluxo/controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,11 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        double nota = 7.0;
+
+        if (nota >= 7) {
+            System.out.println("APROVADO");
+        } else{
+            System.out.println("REPROVADO");
+        }
+    }
+}
