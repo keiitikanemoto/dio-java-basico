00d68f9cf4656e298caae977fd8e8daed3390198
keiitikanemoto <lincolnkanemoto@zohomail.com>
2023-06-01 18:24:42 -0300

    OPerador Ternário e else if

--- ControleDeFluxo/controle-fluxo/src/ResultadoEscolar.java
+++ ControleDeFluxo/controle-fluxo/src/ResultadoEscolar.java
@@ -1,11 +1,19 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        double nota = 7.0;
+        double nota = 3.0;
 
-        if (nota >= 7) {
+        /*if (nota >= 7) {
             System.out.println("APROVADO");
+        } else if (nota >= 5 && nota < 7){
+            System.out.println("RECUPERAÇÃO");
         } else{
             System.out.println("REPROVADO");
-        }
+        }*/
+        
+            String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado"; //operador ternário com else if
+
+            System.out.println(resultado);
+
+        
     }
 }
